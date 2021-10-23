58935d1026c4d819a6ca1a959b51af9ee4fc258c
EMiltonn <emanuelnews@outlook.com>
2021-10-23 16:22:09 -0400

    :wrench: Configuraçoes: rotas e server

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,15 @@
-console.log('Hello Dev!')
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(routes);
+
+app.use(cors());
+
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000!');
+});
